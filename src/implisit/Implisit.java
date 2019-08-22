550e877801f8ed532afa0304b225f84d7b3ee7f6
sumsky108 <sumendrakadek108@gmail.com>
2019-08-22 10:27:58 +0700

    first commit

---
+++ src/implisit/Implisit.java
@@ -0,0 +1,11 @@
+package implisit;
+public class Implisit {
+
+    public static void main(String[] args) {
+        byte a=100;
+        int j=a;
+        System.out.println("sebelum dicasting"+a);
+        System.out.println("sesuadah dicasting"+j);
+    }
+    
+}
